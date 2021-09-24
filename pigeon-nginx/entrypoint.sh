a53f5d444222d6c3714e36b68de713fc3a25f281
claypigeon123 <n.zoltan18@gmail.com>
2021-09-24 22:32:20 +0200

    entry script

---
+++ pigeon-nginx/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+SLEEP_TIME=2
+declare -a DEPENDENCIES=("pigeon-nexus:8081" "pigeon-jenkins-controller:8080")
+for d in ${DEPENDENCIES[@]}; do 
+  echo "waiting for $d to be available";
+  until wget -S -O - http://$d 2>&1 | grep "HTTP/" | awk '{print $2}' | grep "200\|404\|403\|401\|301\|302" &> /dev/null
+  do
+      echo "$d unavailable, sleeping for ${SLEEP_TIME}"
+      sleep "${SLEEP_TIME}"
+  done
+done
+
+/usr/sbin/nginx
