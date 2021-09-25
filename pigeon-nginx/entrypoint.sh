8dd36941765cde69539b8e4de19a492adef23048
claypigeon123 <n.zoltan18@gmail.com>
2021-09-26 01:17:19 +0200

    adding raid manager service

--- pigeon-nginx/entrypoint.sh
+++ pigeon-nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 SLEEP_TIME=2
-declare -a DEPENDENCIES=("pigeon-nexus:8081" "pigeon-jenkins-controller:8080")
+declare -a DEPENDENCIES=("pigeon-nexus:8081" "pigeon-jenkins-controller:8080" "cp-raid-manager-system-controller-app:9000")
 for d in ${DEPENDENCIES[@]}; do 
   echo "waiting for $d to be available";
   until wget -S -O - http://$d 2>&1 | grep "HTTP/" | awk '{print $2}' | grep "200\|404\|403\|401\|301\|302" &> /dev/null
